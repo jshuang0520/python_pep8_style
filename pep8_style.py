902dc36655629195b7e2b386c296222b966637cd
Johnson Huang 黃聖翔 <johnson.huang@gogoro.com>
2020-06-12 10:37:30 +0800

    feat: add reference sites

---
+++ pep8_style.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# python pep8
+"""
+How to Write Beautiful Python Code With PEP 8
+https://realpython.com/python-pep8/
+
+PEP8 PYTHON 編碼規範手冊
+https://cflin.com/wordpress/603/pep8-python%E7%B7%A8%E7%A2%BC%E8%A6%8F%E7%AF%84%E6%89%8B%E5%86%8A
+
+PEP Index > PEP 8 -- Style Guide for Python Code
+https://legacy.python.org/dev/peps/pep-0008/
+"""
